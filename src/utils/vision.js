ced7fa95cf2322709950bdbfb5353c2cd2d2047b
Preston Tom-Ying <preston.tomying@gmail.com>
2024-01-13 18:12:57 -0500

    VISION WORKS

--- src/utils/vision.js
+++ src/utils/vision.js
@@ -1,71 +1,27 @@
-
-// Imports the Google Cloud client libraries
-const vision = require('@google-cloud/vision').v1;
+const vision = require('@google-cloud/vision');
 
 // Creates a client
 const client = new vision.ImageAnnotatorClient();
 
+console.log("HELLO");
 /**
- * TODO(developer): Uncomment the following lines before running the sample.
+ * TODO(developer): Uncomment the following line before running the sample.
  */
-// Bucket where the file resides
-// const bucketName = 'my-bucket';
-// Path to PDF file within bucket
-// const fileName = 'path/to/document.pdf';
-// The folder to store the results
-// const outputPrefix = 'results'
+const fileName = './public/healthCards/healthCard1.jpg';
 
-const gcsSourceUri = `gs://${bucketName}/${fileName}`;
-const gcsDestinationUri = `gs://${bucketName}/${outputPrefix}/`;
+async function someFunction() {
+    try {
+      // Performs text detection on the local file
+      const [result] = await client.textDetection(fileName);
+      const detections = result.textAnnotations;
+      console.log('Text:');
+      detections.forEach(text => console.log(text));
+      
+      // rest of your code using the result
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
 
-const inputConfig = {
-  // Supported mime_types are: 'application/pdf' and 'image/tiff'
-  mimeType: 'application/pdf',
-  gcsSource: {
-    uri: gcsSourceUri,
-  },
-};
-const outputConfig = {
-  gcsDestination: {
-    uri: gcsDestinationUri,
-  },
-};
-const features = [{type: 'DOCUMENT_TEXT_DETECTION'}];
-const request = {
-  requests: [
-    {
-      inputConfig: inputConfig,
-      features: features,
-      outputConfig: outputConfig,
-    },
-  ],
-};
-
-const [operation] = await client.asyncBatchAnnotateFiles(request);
-const [filesResponse] = await operation.promise();
-const destinationUri =
-  filesResponse.responses[0].outputConfig.gcsDestination.uri;
-console.log('Json saved to: ' + destinationUri);
-
-
-
-
-
-
-// Imports the Google Cloud client library
-const vision = require('@google-cloud/vision');
-
-async function setEndpoint() {
-  // Specifies the location of the api endpoint
-  const clientOptions = {apiEndpoint: 'eu-vision.googleapis.com'};
-
-  // Creates a client
-  const client = new vision.ImageAnnotatorClient(clientOptions);
-
-  // Performs text detection on the image file
-  const [result] = await client.textDetection('./resources/wakeupcat.jpg');
-  const labels = result.textAnnotations;
-  console.log('Text:');
-  labels.forEach(label => console.log(label.description));
-}
-setEndpoint();
+  someFunction();
+  console.log("ASD");
